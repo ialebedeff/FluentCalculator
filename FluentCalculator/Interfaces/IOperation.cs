16b7d105a002b18b2a1a2eff74ee2240f5a49b8c
Lebedeff <106642323+ialebedeff@users.noreply.github.com>
2023-02-05 12:52:19 +0300

    Update IOperation.cs

--- FluentCalculator/Interfaces/IOperation.cs
+++ FluentCalculator/Interfaces/IOperation.cs
@@ -1,6 +1,15 @@
 ﻿namespace FluentCalc.Calculator;
 
+/// <summary>
+/// Интерфейс операций
+/// </summary>
 public interface IOperation
 {
+    /// <summary>
+    /// Рассчитать значение на основе предыдущего результата и текущего значения
+    /// </summary>
+    /// <param name="lastValue"></param>
+    /// <param name="currentValue"></param>
+    /// <returns></returns>
     public int Calculate(int lastValue, int currentValue);
 }
