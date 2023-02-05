be2c4098a77153b5092927ea91c1a988e41f7c89
Lebedeff <106642323+ialebedeff@users.noreply.github.com>
2023-02-05 12:52:37 +0300

    Update DevideOperation.cs

--- FluentCalculator/Operations/DevideOperation.cs
+++ FluentCalculator/Operations/DevideOperation.cs
@@ -1,7 +1,16 @@
 ﻿namespace FluentCalc.Calculator;
 
+/// <summary>
+/// Деление
+/// </summary>
 public class DevideOperation : IOperation
 {
+    /// <summary>
+    /// Поделить последний результат на текущее значение
+    /// </summary>
+    /// <param name="lastValue"></param>
+    /// <param name="currentValue"></param>
+    /// <returns></returns>
     public int Calculate(int lastValue, int currentValue)
         => lastValue / currentValue;
 }
