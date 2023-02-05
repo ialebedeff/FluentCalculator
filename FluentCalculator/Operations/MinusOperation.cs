b08c0e711c03245b9fadb1205357431488895704
Lebedeff <106642323+ialebedeff@users.noreply.github.com>
2023-02-05 12:52:50 +0300

    Update MinusOperation.cs

--- FluentCalculator/Operations/MinusOperation.cs
+++ FluentCalculator/Operations/MinusOperation.cs
@@ -1,7 +1,17 @@
 ﻿namespace FluentCalc.Calculator;
 
+/// <summary>
+/// Вычитание
+/// </summary>
 public class MinusOperation : IOperation
 {
+    /// <summary>
+    /// Вычесть из последнего результата текущее значение
+    /// </summary>
+    /// <param name="lastValue"></param>
+    /// <param name="currentValue"></param>
+    /// <returns></returns>
     public int Calculate(int lastValue, int currentValue)
         => lastValue - currentValue;
 }
+
